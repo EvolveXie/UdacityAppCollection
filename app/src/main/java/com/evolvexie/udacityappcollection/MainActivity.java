691d71373b0d2294689cc3e1dd32cbd79dd47b79
evolvexie <evolvexie.dev@gmail.com>
2017-03-10 21:26:08 +0800

    change the way to use the text size,color,button style add cool library - Toasty

--- app/src/main/java/com/evolvexie/udacityappcollection/MainActivity.java
+++ app/src/main/java/com/evolvexie/udacityappcollection/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import es.dmoral.toasty.Toasty;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,19 +19,22 @@ public class MainActivity extends AppCompatActivity {
         int id = view.getId();
         switch (id) {
             case R.id.hotMoviesBtn:
-                Toast.makeText(this,"启动应用：热门电影",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "启动应用：热门电影", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.stockEagleBtn:
-                Toast.makeText(this,"启动应用：股票雄鹰",Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this,"启动应用：股票雄鹰",Toast.LENGTH_SHORT).show();
+                Toasty.success(this, "启动应用：股票雄鹰", Toast.LENGTH_SHORT, true).show();
                 break;
             case R.id.xyzReaderBtn:
-                Toast.makeText(this,"启动应用：XYZ阅读器",Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this,"启动应用：XYZ阅读器",Toast.LENGTH_SHORT).show();
+                Toasty.info(this, "启动应用：XYZ阅读器", R.drawable.ic_check_white_48dp).show();
                 break;
             case R.id.newestNewsBtn:
-                Toast.makeText(this,"启动应用：最新闻",Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this, "启动应用：最新闻", Toast.LENGTH_SHORT).show();
+                Toasty.normal(this,"启动应用：最新闻",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.graduationProjectBtn:
-                Toast.makeText(this,"启动应用：毕业设计",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "启动应用：毕业设计", Toast.LENGTH_SHORT).show();
                 break;
             default:
                 break;
